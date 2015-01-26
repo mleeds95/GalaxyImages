ff1c3df4039e6059d8dff448a7263ace45afee6d
Matthew Leeds <mwl458@gmail.com>
2015-01-26 12:45:25 -0600

    Switch to 50 kpc, etc.

--- ExtractGalaxy.py
+++ ExtractGalaxy.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python
 
+###################################################################
+#
+# File: ExtractGalaxy.py
+# Author: Matthew Leeds
+# Last Edit: 2015-01-20
+# Purpose: Use PyNBody to extract the galaxy and its surroundings
+# for a certain radius.
+#
+###################################################################
+
 import pynbody
 
 def main():
+    pynbody.config['verbose'] = True
     print "Loading g15784 at t=1024"
     galaxy = pynbody.load("./MUGS-g15784/g15784.01024")
     print "ngas = %e, ndark = %e, nstar = %e"%(len(galaxy.g), len(galaxy.d), len(galaxy.s))
@@ -12,9 +23,12 @@ def main():
     h1 = galaxy.halos()[1]    
     print "ngas = %e, ndark = %e, nstar = %e"%(len(h1.g), len(h1.d), len(h1.s))
     print "Centering the simulation around the main halo"
-    hcenter = pynbody.analysis.halo.center(h1, mode='com')
+    hcenter = pynbody.analysis.halo.center(h1, mode='hyb')
     print(hcenter)
-    sphere = h1[pynbody.filt.Sphere('100 kpc')]
-    print(sphere)
-
+    print "Filtering out a sphere"
+    sphere = h1[pynbody.filt.Sphere('50 kpc')]
+    #print(sphere)
+    print "Writing the sphere to the disk in Tipsy format"
+    sphere.write(filename=sphere.filename, fmt=pynbody.tipsy.TipsySnap)
+    
 main()
