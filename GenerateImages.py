5df1fd0928282034a01ff77c6df5da2a7b54de7e
mleeds95 <mwl458@gmail.com>
2015-03-26 13:18:41 -0500

    GenerateImages.py is working (for one timestep).

--- GenerateImages.py
+++ GenerateImages.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python
 
-###############################################################################
+################################################################################
 #
 # File: GenerateImages.py
 # Author: Matthew Leeds <mwleeds@crimson.ua.edu>
 # Last Edit: 2015-03-25
 # Purpose: Generate color RGB images in JPEG format from Sunrise's broadband
-# output using Patrik's make_color module. Please make sure this code:
-# https://bitbucket.org/lutorm/python
-# is in your PATH and you have astropy and numpy installed.
+# output using Patrik's make_color module. Please run this:
+# hg clone https://bitbucket.org/lutorm/python
+# rename that folder 'patrikpython', add an empty '__init__.py' file, and put
+# it in your PYTHONPATH. Also have numpy and astropy installed.
+# Config parameters should be set in 'config.ini' under 'Generate Images'.
+# Image files will be written to the same directory as the input fits file.
 # Usage: 'python GenerateImages.py <broadband-outfile>.fits'
 #
-###############################################################################
+################################################################################
 
 import sys
 from astropy.io import fits
-from make_color import make_image
+from patrikpython import make_color
 from numpy import transpose
 import ConfigParser
 
@@ -24,25 +27,26 @@ if len(sys.argv) != 2:
     sys.exit(1)
 
 CONFIG_FILE = "config.ini"
-BANDS=(3,2,1) # temporarily hardcoded
 
 def main(args):
+    fitsFilename = args[0]
     config = ConfigParser.SafeConfigParser()
     sys.stdout.write("Reading " + CONFIG_FILE + "\n")
     config.read(CONFIG_FILE)
     if not config.has_section("Generate Images"):
         sys.stderr.write("Error: No \"Generate Images\" section found in " + CONFIG_FILE + ".\n")
         sys.exit(1)
-#TODO TODO TODO TODO remove dependence on camposFile, etc.
-    broadbandFile = fits.open(args[0])
-    for i, line in enumerate(camposFile):
-        im = transpose(broadbandFile['CAMERA' + str(i) + '-BROADBAND'].data, axes=(1,2,0))
-        img = make_image(im, band=BANDS, scale='auto', autopercentile=AUTOPERCENTILE, return_jpeg=True) 
-        filename = args[0] + '_' + str(i) + '.jpg'
-        with open(filename, 'w') as f:
-            f.write(img)
-        print(filename + ': ' + line)
-    broadbandFile.close()
+    sys.stdout.write("Reading " + fitsFilename + "\n")
+    f = fits.open(fitsFilename)
+    numCameras = f['MCRX'].header['N_CAMERA']
+    filters = [f['FILTERS'].data[i][0] for i in range(len(f['FILTERS'].data))]
+    sys.stdout.write("Generating RGB images for " + str(numCameras) + " perspectives with " + str(len(filters)) + " filters.\n")
+    f.close()
+    autoPercentile = float(config.get('Generate Images', 'AUTOPERCENTILE'))
+    filterSets = config.get('Generate Images', 'FILTER_SETS').split(';')
+    for filterSet in filterSets:
+        make_color.write_all(fitsFilename, '-BROADBAND', fitsFilename[:-5] + '-%d.jpg', 
+                             band=filterSet, scale='auto', autopercentile=autoPercentile, overwrite=True)
     sys.exit(0)
 
 if __name__=='__main__':
