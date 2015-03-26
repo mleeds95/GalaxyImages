778b6df509eaab586e05aee722b01b52f0877860
mleeds95 <mwl458@gmail.com>
2015-03-26 18:36:35 -0500

    Generalize GenerateImages.py to multiple timesteps.

--- GenerateImages.py
+++ GenerateImages.py
@@ -11,43 +11,73 @@
 # rename that folder 'patrikpython', add an empty '__init__.py' file, and put
 # it in your PYTHONPATH. Also have numpy and astropy installed.
 # Config parameters should be set in 'config.ini' under 'Generate Images'.
-# Image files will be written to the same directory as the input fits file.
-# Usage: 'python GenerateImages.py <broadband-outfile>.fits'
 #
 ################################################################################
 
+import os
 import sys
+import re
 from astropy.io import fits
 from patrikpython import make_color
 from numpy import transpose
-import ConfigParser
-
-if len(sys.argv) != 2:
-    print "Usage: 'python GenerateImages.py <broadband-outfile>.fits'"
-    sys.exit(1)
+from ConfigParser import SafeConfigParser, NoOptionError
 
 CONFIG_FILE = "config.ini"
+SECTION_NAME = "Generate Images"
 
-def main(args):
-    fitsFilename = args[0]
-    config = ConfigParser.SafeConfigParser()
+def main():
+    config = SafeConfigParser()
     sys.stdout.write("Reading " + CONFIG_FILE + "\n")
     config.read(CONFIG_FILE)
-    if not config.has_section("Generate Images"):
-        sys.stderr.write("Error: No \"Generate Images\" section found in " + CONFIG_FILE + ".\n")
+    if not config.has_section(SECTION_NAME):
+        sys.stderr.write("Error: No '" + SECTION_NAME + "' section found in " + CONFIG_FILE + ".\n")
         sys.exit(1)
-    sys.stdout.write("Reading " + fitsFilename + "\n")
-    f = fits.open(fitsFilename)
-    numCameras = f['MCRX'].header['N_CAMERA']
-    filters = [f['FILTERS'].data[i][0] for i in range(len(f['FILTERS'].data))]
-    sys.stdout.write("Generating RGB images for " + str(numCameras) + " perspectives with " + str(len(filters)) + " filters.\n")
-    f.close()
-    autoPercentile = float(config.get('Generate Images', 'AUTOPERCENTILE'))
-    filterSets = config.get('Generate Images', 'FILTER_SETS').split(';')
-    for filterSet in filterSets:
-        make_color.write_all(fitsFilename, '-BROADBAND', fitsFilename[:-5] + '-%d.jpg', 
-                             band=filterSet, scale='auto', autopercentile=autoPercentile, overwrite=True)
+    galaxyName = config.get(SECTION_NAME, "GALAXY_NAME") 
+    os.chdir(config.get(SECTION_NAME, "SUNRISE_DIR"))
+    listOfTimesteps = []
+    try:
+        timeStep = config.get(SECTION_NAME, "TIME_STEP")
+        if len(timeStep) > 0: listOfTimesteps.append(timeStep)
+    except NoOptionError:
+        pass
+    # If a time step wasn't specified, look for them.
+    if len(listOfTimesteps) == 0:
+        for f in os.listdir("."):
+            if re.match(r"^" + galaxyName + "-\d+-sunrise$", f) != None:
+                listOfTimesteps.append(f.split("-")[1])
+    # Make images for every perspective and filter set combination for every time step.
+    outFolder = config.get(SECTION_NAME, "IMAGE_DIR")
+    for timeStep in listOfTimesteps:
+        folderName = galaxyName + "-" + timeStep + "-sunrise"
+        fitsFilename = ""
+        # Find the broadband FITS file for this time step.
+        for f in os.listdir(folderName):
+            #TODO improve regex
+            if re.match(r"^" + galaxyName + "\." + timeStep + ".*\.broadband(-redshift)?\.fits$", f) != None:
+                fitsFilename = f 
+                break
+        if len(fitsFilename) == 0:
+            sys.stderr.write("Error: unable to find broadband FITS file for " + galaxyName + "-" + timeStep + "\n")
+            continue
+        sys.stdout.write("Reading " + fitsFilename + "\n")
+        f = fits.open(fitsFilename)
+        numCameras = f["MCRX"].header["N_CAMERA"]
+        filters = [f["FILTERS"].data[i][0] for i in range(len(f["FILTERS"].data))]
+        f.close()
+        filterSets = []
+        filterNameSets = config.get(SECTION_NAME, "FILTER_NAMES").split(";")
+        # Convert the filter names to indices.
+        for filterNames in filterNameSets:
+            filterIndices = [str(filters.index(filterName + ".res")) for filterName in filterNames.split(",")]
+            filterSets.append(",".join(filterIndices))
+        sys.stdout.write("Generating RGB images for " + str(numCameras) + " perspectives with " + str(len(filters)) + " filters.\n")
+        autoPercentile = float(config.get(SECTION_NAME, "AUTOPERCENTILE"))
+        # For each specified set of filters, make images from every perspective.
+        for i, filterSet in enumerate(filterSets):
+            outFile = fitsFilename[:-15] + "-filter" + str(i)
+            make_color.write_all(fitsFilename, "-BROADBAND", outFolder + outFile + "-camera%d.jpg", 
+                                 band=filterSet, scale="auto", autopercentile=autoPercentile, overwrite=True)
     sys.exit(0)
 
-if __name__=='__main__':
-    main(sys.argv[1:])
+if __name__=="__main__":
+    main()
